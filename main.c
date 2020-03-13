b9cb4539ee2faeeea679f54dbb93b3ec8e47f3eb
valentino_losito <Valentino.LOSITO@cpnv.ch>
2020-03-13 15:02:03 +0100

    ajoute du choix au menu principal+choix authenfitication
    authentification pas terminé

--- main.c
+++ main.c
@@ -11,25 +11,73 @@
  *  \return fin du programme.
  */
 
+char psuedo;
+int choixmenuprincipal,choixmenujouer,choixmenuauthentification;
+
+
+
 void afficherTitre() {
     printf("----Bataille Navale----\n");
 }
-void afficherMenuPrincipal(){
+
+
+int afficherMenuPrincipal() {
     printf("1.JOUER\n");
     printf("2.AIDE\n");
     printf("3.LISTE DES SCORES\n");
-    printf("4.QUITTER LE JEU\n");
+    printf("4.QUITTER LE JEU\n\n");
+    printf("CHOIX:");
+    scanf("%d", &choixmenuprincipal);
+    printf("\n");
+    return choixmenuprincipal;
+}
+int afficherchoixdesiredansmenuprincipal(int choixmenuprincipal) {
+    switch (choixmenuprincipal) {
+        case 1:
+            printf("----JOUER----\n");
+            printf("1.AUTHENTIFICATION\n");
+            printf("2.RETOUR\n\n");
+            printf("CHOIX:");
+            scanf("%d", &choixmenujouer);
+            break;
+        case 2:
+            printf("----AIDE----\n");
+            printf("ecrire l'aide du jeu");
+            break;
+
+    }
+}
+
+
+int afficherchoixdesiredansmenujouer(int choixmenujouer) {
+    switch(choixmenujouer){
+        case 1:
+            printf("----AUTHENTIFICATION----\n");
+            printf("psuedo 1:");
+            scanf("%s",&psuedo);
+            break;
+    }
 }
 
 int main() {
 
+
+
+
     // afficher le titre
     afficherTitre();
 
-    //afficher le Menu Principal
-    afficherMenuPrincipal();
+    //afficher le Menu Principal + choix au menu principal
+    choixmenuprincipal = afficherMenuPrincipal();
 
-    //
+    //afficher le choix desiré + choix au menu jouer
+    choixmenujouer = afficherchoixdesiredansmenuprincipal(choixmenuprincipal);
+
+    //afficher l'authentification + entrer son psuedo
+    choixmenuauthentification = afficherchoixdesiredansmenujouer(choixmenujouer);
+
+
+
 
     return 0;
 }
