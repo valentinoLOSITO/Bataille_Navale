55f741fcbc3180a6f82dbc90d580aca09235e83f
valentino_losito <Valentino.LOSITO@cpnv.ch>
2020-03-13 14:05:01 +0100

    insertion titre et menu principal
    titre inseré ainsi que le menu principal

--- main.c
+++ main.c
@@ -1,6 +1,35 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+
+#pragma execution_character_set ("utf-8")
+
+/** \author Valentino losito
+ *  \date 13.03.2020
+ *  \version 0.1
+ *  \description jeu de la bataille navale
+ *  \return fin du programme.
+ */
+
+void afficherTitre() {
+    printf("----Bataille Navale----\n");
+}
+void afficherMenuPrincipal(){
+    printf("1.JOUER\n");
+    printf("2.AIDE\n");
+    printf("3.LISTE DES SCORES\n");
+    printf("4.QUITTER LE JEU\n");
+}
 
 int main() {
-    printf("----Bataille Navale----\n");
+
+    // afficher le titre
+    afficherTitre();
+
+    //afficher le Menu Principal
+    afficherMenuPrincipal();
+
+    //
+
     return 0;
 }
